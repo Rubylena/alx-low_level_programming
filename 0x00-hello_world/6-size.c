81f7e75937d1e2d8143fb70e3c0133bc3b084cfb
Ruby <graceffiong@gmail.com>
2022-03-10 21:20:12 +0000

    size of

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -11,10 +11,10 @@ int main(void)
 	char c;
 	float f;
 
-	printf("Size of a char: %lu byte(s)\n", (nsigned long)sizeof(c);
-	printf("Size of a float: %lu byte(s)\n", (nsigned long)sizeof(f);
-	printf("Size of a int: %lu byte(s)\n", (nsigned long)sizeof(i);
-	printf("Size of a long int: %lu byte(s)\n", (nsigned long)sizeof(i2);
-	printf("Size of a long long int: %lu byte(s)\n", (nsigned long)sizeo((i3);
+	printf("Size of a char: %lu byte(s)\n", (unsigned long)sizeof(c);
+	printf("Size of a float: %lu byte(s)\n", (unsigned long)sizeof(f);
+	printf("Size of a int: %lu byte(s)\n", (unsigned long)sizeof(i);
+	printf("Size of a long int: %lu byte(s)\n", (unsigned long)sizeof(i2);
+	printf("Size of a long long int: %lu byte(s)\n", (unsigned long)sizeo((i3);
 	return (0);
 }
