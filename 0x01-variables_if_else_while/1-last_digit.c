c8ab996097fcadea7f86d058944d5bbb42d92cbd
Ruby <graceffiong@gmail.com>
2022-03-11 20:52:32 +0000

    task 1

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -12,9 +12,9 @@ int main(void)
 	int n;
 	int ld;
 
-	ld = n % 10;
 	srand(time(0));
 	n = rand() - RAND_MAX / 2;
+	ld = n % 10;
 	if (ld > 5)
 	{
 		printf("Last digit of %d is %d and is greater than 5\n", n, ld);
