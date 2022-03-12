8c5d3a50f36a04ba7a54b4dc632f597ada696f0a
Ruby <graceffiong@gmail.com>
2022-03-12 00:21:27 +0000

    task 10

---
+++ 0x01-variables_if_else_while/10-print_comb3.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ * Description - program that prints all possible different
+ * combinations of two digits
+ *
+ * Return: 0
+ */
+
+int main(void)
+{
+	for (int i = 0; i < 10; i++)
+{
+	for (int l = i + 1; l < 10; l++)
+	{
+		putchar(i + '0');
+		putchar(l + '0');
+		if (i == 8 && l == 9)
+			continue;
+			putchar(',');
+			putchar(' ');
+	}
+}
+	putchar('\n');
+	return (0);
+}
