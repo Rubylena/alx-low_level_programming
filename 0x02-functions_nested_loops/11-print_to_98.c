43d3d90cce8bbe354bac442e9a04a26d5a96d708
Ruby <graceffiong@gmail.com>
2022-03-15 23:24:25 +0100

    Update 11-print_to_98.c

--- 0x02-functions_nested_loops/11-print_to_98.c
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -11,18 +11,13 @@ void print_to_98(int n)
 	if (n >= 98)
 	{
 		for (n; n > 98; n--)
-		{
 			printf("%d, ", n);
-		}
 		printf("%d\n", n);
 	}
 	else
 	{
 		for (n; n < 98; n++)
-		{
 			printf("%d, ", n);
-		}
 		printf("%d\n", n);
 	}
-	return (0);
 }
