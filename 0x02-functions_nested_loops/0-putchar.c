89296c31666b27df59a9e3e338403f571f5d80c5
Ruby <graceffiong@gmail.com>
2022-03-15 13:06:49 +0000

    header file and task 0

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,16 @@
+#include "main.h"
+/**
+ * main - Entry point
+ * Description - Write a program that prints _putchar
+ * @c: input
+ * Return: 0
+ */
+int _putchar(char *c);
+
+int main(void)
+{
+	char *c = "_putchar";
+
+	_putchar(c);
+	return (0);
+}
---
+++ 0x02-functions_nested_loops/main.h
@@ -0,0 +1,17 @@
+#ifndef _MAIN_H_
+#define _MAIN_H_
+#include <stdio.h>
+
+/**
+ * _putchar - writes the character c to stdout
+ * @c: The character to print
+ * Return: 0
+ */
+
+int _putchar(char *c)
+{
+	printf("%s\n", c);
+	return (0);
+}
+
+#endif
