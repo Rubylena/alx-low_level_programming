73594d2e3fa95db0904f18622a7399f6f7c26e43
Ruby <graceffiong@gmail.com>
2022-03-15 18:29:47 +0000

    task 7

---
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -0,0 +1,12 @@
+#include "main.h"
+/**
+ * print_last_digit - check the code
+ * @n: int type character
+ * Return: 0
+ */
+int print_last_digit(int n)
+{
+	_putchar((n % 10) + '0');
+	_putchar('\n');
+	return (0);
+}
