e66d0f4ba8ded05c9f4a5462a3ac85d07ac62c58
Ruby <graceffiong@gmail.com>
2022-03-15 15:56:13 +0000

    task 3

---
+++ 0x02-functions_nested_loops/3-islower.c
@@ -0,0 +1,15 @@
+#include "main.h"
+/**
+ * _islower - check the code
+ * @c: char type character
+ * Return: 0
+ */
+int _islower(nt c)
+{
+	int c
+		;
+	c = islower(c);
+	_putchar(c + '0');
+	_putchar('\n');
+	return (0);
+}
