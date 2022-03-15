28e51e84db56b96734d9a107a674dc34e85bc778
Ruby <graceffiong@gmail.com>
2022-03-15 22:20:43 +0000

    task 11

---
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -0,0 +1,27 @@
+#include "main.h"
+/**
+ * print_to_98 - check the code
+ * @n: int type character
+ *
+ * Return: 0
+ */
+void print_to_98(int n)
+{
+	if (n >= 98)
+	{
+		for (n; n > 98; n--)
+		{
+			printf("%d, ", n);
+		}
+		printf("%d\n", n);
+	}
+	else
+	{
+		for (n; n < 98; n++)
+		{
+			printf("%d, ", n);
+		}
+		printf("%d\n", n);
+	}
+	return (0);
+}
