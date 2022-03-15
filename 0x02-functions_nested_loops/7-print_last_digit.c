964b245d0fe4df4d55a9684710cc33f933066e37
Ruby <graceffiong@gmail.com>
2022-03-15 19:56:27 +0100

    Update 7-print_last_digit.c

--- 0x02-functions_nested_loops/7-print_last_digit.c
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -6,16 +6,14 @@
  */
 int print_last_digit(int n)
 {
-	int num = n % 10;
-
 	if (n < 0)
 	{
-		_putchar((num * -1) + '0');
-		return (-num);
+		_putchar(((n % 10) * -1) + '0');
+		return (-n);
 	}
 	else
 	{
-		_putchar(num + '0');
-		return (num);
+		_putchar((n % 10) + '0');
+		return (n);
 	}
 }
