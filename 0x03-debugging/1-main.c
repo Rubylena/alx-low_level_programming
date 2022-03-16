3e94148b9c06451ff38f30cbb246eee2bd76fdb7
Ruby <graceffiong@gmail.com>
2022-03-16 12:55:41 +0000

    task 1

--- 0x03-debugging/1-main.c
+++ 0x03-debugging/1-main.c
@@ -2,7 +2,6 @@
 
 /**
 * main - causes an infinite loop
-* Description - '\\o/\n"' printf
 * Return: 0
 */
 
@@ -15,11 +14,11 @@ int main(void)
 	i = 0;
 
 /**
-* while - (i < 10)
-* {
-* putchar(i);
-* }
-*/
+ * while - (i < 10)
+ * {
+ * putchar(i);
+ * }
+ */
 	printf("Infinite loop avoided! \\o/\n");
 
 	return (0);
