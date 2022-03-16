553dbbf085c02e7832aa1c42595082ffbc470403
Ruby <graceffiong@gmail.com>
2022-03-16 11:59:12 +0000

    task o0

---
+++ 0x03-debugging/main.h
@@ -0,0 +1,13 @@
+#ifndef _MAIN_H_
+#define _MAIN_H_
+#include <unistd.h>
+
+/**
+ * _putchar - writes the character c to stdout
+ * @i: The int to print
+ * Return: 0
+ */
+
+void positive_or_negative(int i);
+
+#endif /* MAIN_H */
