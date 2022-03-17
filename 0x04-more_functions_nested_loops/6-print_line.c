2a8f5858bf6dd39d8d80d44a2284374f4f50b7e8
Ruby <graceffiong@gmail.com>
2022-03-17 18:37:49 +0000

    task 6

---
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -0,0 +1,28 @@
+#include "main.h"
+
+/**
+ * print_line - Entry point
+ * @n: number of times
+ *
+ * Return: 0
+ */
+void print_line(int n)
+{
+	int i;
+
+	for (i = 0; i <= n; i++)
+	{
+		if (n > 0)
+		{
+			_putchar('_');
+		}
+		else
+		{
+			_putchar('\n');
+		}
+	}
+	if (n > 0)
+	{
+		_putchar('\n');
+	}
+}
