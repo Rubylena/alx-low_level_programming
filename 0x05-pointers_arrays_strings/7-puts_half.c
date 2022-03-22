313bb36d74b149501df199268251f065c36b18a5
Ruby <graceffiong@gmail.com>
2022-03-22 00:53:35 +0000

    trail

---
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -0,0 +1,31 @@
+#include "main.h"
+
+/**
+ * puts_half - print a string to stdout
+ * @str: char array string type
+ * Description: Can only use _putchar
+ */
+
+void puts_half(char *str)
+{
+	int i;
+	int n;
+	char len = str[i];
+
+	for (i = 0; len != '\0'; i++)
+	{
+		if ((len % 2) == 0)
+		{
+			n = len / 2;
+		}
+		else
+		{
+			n = (len + 1) / 2;
+		}
+		for (i = n; i < len; i++)
+		{
+			_putchar(len);
+		}
+		_putchar('\n');
+	}
+}
