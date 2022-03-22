894df77e83a413bb03ca49427abe52fac1d7c92f
Ruby <graceffiong@gmail.com>
2022-03-22 00:27:27 +0000

    task 4

---
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,22 @@
+#include "main.h"
+
+/**
+ * print_rev - print a string in reverse
+ * @s: char array string type
+ */
+
+void print_rev(char *s)
+{
+	int i;
+	int j;
+
+	for (i = 0; s[i] != '\0'; i++)
+	{
+	}
+
+	for (i = j - 1; i >= 0; i--)
+	{
+		_putchar(s[i]);
+	}
+	_putchar('\n');
+}
