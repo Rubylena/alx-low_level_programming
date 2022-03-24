8961969090cfd9a68a08e862931c84ce552d7ba8
Ruby <graceffiong@gmail.com>
2022-03-24 07:13:55 +0000

    initialize and task 0

---
+++ 0x06-pointers_arrays_strings/main.h
@@ -0,0 +1,15 @@
+#ifndef _MAIN_H_
+#define _MAIN_H_
+#include <unistd.h>
+
+/**
+ * _putchar - writes the character c to stdout
+ * @c: The int to print
+ * Return: 0
+ */
+
+int _putchar(char c);
+char *_strcat(char *dest, char *src);
+
+
+#endif /* MAIN_H */
