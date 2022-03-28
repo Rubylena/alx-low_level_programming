e29941947599a50e47a83e7285ed509232e61802
Ruby <graceffiong@gmail.com>
2022-03-28 22:39:42 +0100

    Update 1-memcpy.c

--- 0x07-pointers_arrays_strings/1-memcpy.c
+++ 0x07-pointers_arrays_strings/1-memcpy.c
@@ -9,13 +9,13 @@
  *
  * Return: A pointer to the filled memory area @dest.
  */
-char *_memcpy(char *s, char b,  unsigned int n)
+char *_memcpy(char *dest, char *src,  unsigned int n)
 {
 	unsigned int i;
 
 	for (i = 0; i < n; i++)
 	{
-		s[i] = b;
+		dest[i] = src[i];
 	}
-	return (s);
+	return (dest);
 }
