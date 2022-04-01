121b48f4d1782c35bdc7df4e928c45248e9d0ad8
Ruby <graceffiong@gmail.com>
2022-04-01 22:20:42 +0000

    complete

--- 0x0A-argc_argv/3-mul.c
+++ 0x0A-argc_argv/3-mul.c
@@ -1,6 +1,5 @@
 #include <stdio.h>
 #include <stdlib.h>
-#include "main.h"
 /**
  * main - multiplies two numbers.
  * @argc: number of command line arguments.
@@ -9,18 +8,11 @@
  */
 int main(int argc, char *argv[])
 {
-	if (argc == 3)
-	{
-		for (i = 1; i < argc; i++)
-		{
-			mul *= atoi(argv[i]);
-			printf("%d\n", argv[i]);
-		}
-	}
-	else
+	if (argc != 3)
 	{
 		printf("Error\n");
 		return (1);
 	}
+	printf("%d\n", atoi(argv[1]) * atoi(argv[2]));
 	return (0);
 }
