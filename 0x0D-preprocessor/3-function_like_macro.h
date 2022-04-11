ca3d92c996da13bea2ba363665c7664c58ab0087
Ruby <graceffiong@gmail.com>
2022-04-11 19:45:09 +0100

    Update 3-function_like_macro.h

--- 0x0D-preprocessor/3-function_like_macro.h
+++ 0x0D-preprocessor/3-function_like_macro.h
@@ -1,5 +1,5 @@
-#ifndef _3_FUNCTION_LIKE_MACRO_H_
-#define _3_FUNCTION_LIKE_MACRO_H_
+#ifndef FUNCTION_LIKE_MACRO_H_
+#define FUNCTION_LIKE_MACRO_H_
 #define ABS(x) ((x) <= 0 ? (-x) : (x))
 
-#endif /* _3_FUNCTION_LIKE_MACRO_H_ */
+#endif /* FUNCTION_LIKE_MACRO_H_ */
