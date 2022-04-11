0b11ffe039b9b859e34bbba28cb6dd8786b0070a
Ruby <graceffiong@gmail.com>
2022-04-11 20:27:11 +0100

    Update 1-init_dog.c

--- 0x0E-structures_typedef/1-init_dog.c
+++ 0x0E-structures_typedef/1-init_dog.c
@@ -1,6 +1,5 @@
 #include "dog.h"
 #include <stdlib.h>
-#include <stdio.h>
 
 /**
  * init_dog - initialize a variable of type struct dog.
@@ -8,7 +7,6 @@
  * @name: The name of the dog.
  * @age: The age of the dog.
  * @owner: The owner of the dog.
- * Return: (d)
  */
 
 void init_dog(struct dog *d, char *name, float age, char *owner)
@@ -16,9 +14,9 @@ void init_dog(struct dog *d, char *name, float age, char *owner)
 	struct dog *d;
 
 	d = malloc(sizeof(struct dog));
-
+	if (d == NULL)
+		return (NULL);
 	d->name = name;
 	d->age = age;
 	d->owner = owner;
-	return (d);
 }
