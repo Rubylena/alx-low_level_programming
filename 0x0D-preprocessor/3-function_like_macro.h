09be36a40595e9683929f8619cce583b45987743
Ruby <graceffiong@gmail.com>
2022-04-11 19:46:16 +0100

    Update 3-function_like_macro.h

--- 0x0D-preprocessor/3-function_like_macro.h
+++ 0x0D-preprocessor/3-function_like_macro.h
@@ -1,5 +1,11 @@
 #ifndef FUNCTION_LIKE_MACRO_H_
 #define FUNCTION_LIKE_MACRO_H_
 #define ABS(x) ((x) <= 0 ? (-x) : (x))
+/*
+ * File: 3-function_like_macro.h
+ * Auth: Brennan D Baraban
+ * Desc: Header file that defines a function-like macro ABS(x)
+ *       that computes the absolute value of a number x.
+ */
 
 #endif /* FUNCTION_LIKE_MACRO_H_ */
