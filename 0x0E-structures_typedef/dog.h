dbfa4bd7bb84d7e03ee26728d928fe726ffd5667
Ruby <graceffiong@gmail.com>
2022-04-11 21:11:34 +0000

    dog.h file

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -13,6 +13,11 @@ struct dog
 	char *owner;
 	float age;
 };
+/*
+ * dog_t - typedef of struct dog.
+ */
+
+typedef struct dog dog_t;
 
 void init_dog(struct dog *d, char *name, float age, char *owner);
 void print_dog(struct dog *d);
