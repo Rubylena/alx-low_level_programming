de91ffd09bf2ae7e2d1facc059a78f1bf6326552
Ruby <graceffiong@gmail.com>
2022-04-15 18:13:50 +0100

    Update variadic_functions.h

--- 0x10-variadic_functions/variadic_functions.h
+++ 0x10-variadic_functions/variadic_functions.h
@@ -3,7 +3,7 @@
 
 int _putchar(char c);
 int sum_them_all(const unsigned int n, ...);
-void print_number(const char *seperator, const unsigned int n, ...);
+void print_numbers(const char *seperator, const unsigned int n, ...);
 void print_strings(const char *seperator, const unsigned int n, ...);
 void print_all(const char * const format, ...);
 
