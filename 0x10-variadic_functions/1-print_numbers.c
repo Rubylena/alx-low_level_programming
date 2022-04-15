10c1c4947c14ce38315e220c6aeab4303790b4a8
Ruby <graceffiong@gmail.com>
2022-04-15 16:56:59 +0000

    task 1

---
+++ 0x10-variadic_functions/1-print_numbers.c
@@ -0,0 +1,25 @@
+#include "variadic_functions.h"
+#include <stdio.h>
+#include <stdarg.h>
+
+/**
+ * print_numbers - printout numbers
+ * @seperator: character used to seperate integer
+ * @n: no of integer in array
+ */
+void print_numbers(const char *seperator, const unsigned int n, ...)
+{
+	va_list aps;
+	unsigned int i;
+
+	va_start(aps, n);
+	for (i = 0; i < n; i++)
+	{
+		printf("%d", va_args(ap, int));
+/* if n not equals to n - 1, like n = 5, n-1 = 4*/
+		if (i != (n - 1) && seperator != NULL)
+			printf("%s", seperator;
+	}
+	putchar('\n);
+	va_end(aps);
+}
