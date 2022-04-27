ec638eacce756079e7910d6dde4fa4e44b3bde48
Ruby <graceffiong@gmail.com>
2022-04-27 16:25:22 +0000

    task 5

--- 0x13-more_singly_linked_lists/5-free_listint2.c
+++ 0x13-more_singly_linked_lists/5-free_listint2.c
@@ -1,25 +1,23 @@
 #include "lists.h"
 
 /**
- * free_listint2 - Frees a listint_t list.
- * @head: A pointer to the address of the
- *        head of the listint_t list.
- *
- * Description: Sets the head to NULL.
- */
+* free_listint2 - frees a listint_t list
+* sets the head to NULL
+* @head: pointer to pointer to head node
+*
+* Return: no return
+*/
 void free_listint2(listint_t **head)
 {
-	listint_t *tmp;
+	listint_t *temp;
 
-	if (*head == NULL)
-		return;
-
-	while (*head)
+	if (head)
 	{
-		tmp = (*head)->next;
-		free(*head);
-		*head = tmp;
+		while (*head)
+		{
+			temp = *head;
+			*head = temp->next;
+			free(temp);
+		}
 	}
-
-	head = NULL;
 }
