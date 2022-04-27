d5502c2bf5d711ef208a27afad268b689833a176
Ruby <graceffiong@gmail.com>
2022-04-27 13:02:07 +0000

    task 3

--- 0x13-more_singly_linked_lists/2-add_nodeint.c
+++ 0x13-more_singly_linked_lists/2-add_nodeint.c
@@ -14,10 +14,9 @@ listint_t *add_nodeint(listint_t **head, const int n)
 {
 	listint_t *new;
 
+	new = (listint_t *)malloc(sizeof(listint_t));
 	if (new == NULL)
 		return (NULL);
-
-	new = (listint_t *)malloc(sizeof(listint_t));
 
 	new->n = n;
 	new->next = *head;
