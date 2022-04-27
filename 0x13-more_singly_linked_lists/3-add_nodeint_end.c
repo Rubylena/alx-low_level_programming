a3fe84f7f8a05480a34a364e2d9f6baf5dae0b07
Ruby <graceffiong@gmail.com>
2022-04-27 12:24:50 +0100

    Update 3-add_nodeint_end.c

--- 0x13-more_singly_linked_lists/3-add_nodeint_end.c
+++ 0x13-more_singly_linked_lists/3-add_nodeint_end.c
@@ -15,10 +15,10 @@ listint_t *add_nodeint_end(listint_t **head, const int n)
 	listint_t *new;
 
 	new = (listint_t *)malloc(sizeof(listint_t));
+	if (new == NULL)
+		return (NULL);
 
-	new->n = n;
-	new->next = *head;
-	*head= new;
-
-	return (new);
+	new = *head;
+	*head = (*head)->next;
+	free(new);
 }
