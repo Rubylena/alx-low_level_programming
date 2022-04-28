bf831d7b0bfca0b81d197e0b51bd1d9a811537df
Ruby <graceffiong@gmail.com>
2022-04-28 21:04:39 +0100

    Update 0-binary_to_uint.c

--- 0x14-bit_manipulation/0-binary_to_uint.c
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -12,7 +12,7 @@ unsigned int binary_to_uint(const char *b)
 
 	if (b == NULL)
 		return (0);
-	while (b[i])
+	while (b[i] != '\0')
 	{
 		/* if (b[i] < '0' || b[i] > '1') */
 		if (b[i] != '0' && b[i] != '1')
