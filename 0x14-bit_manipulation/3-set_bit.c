3a9b23091926c4306071366172ed0a988d72845a
Ruby <graceffiong@gmail.com>
2022-04-28 21:32:09 +0000

    task 3

--- 0x14-bit_manipulation/3-set_bit.c
+++ 0x14-bit_manipulation/3-set_bit.c
@@ -13,7 +13,7 @@ int set_bit(unsigned long int *n, unsigned int index)
 		return (-1);
 
 	/*1UL unsigned long int, put one at the index indicated*/
-	*n |= (1UL << index);
+	*n |= 1UL << index;
 	return (1);
 }
 
