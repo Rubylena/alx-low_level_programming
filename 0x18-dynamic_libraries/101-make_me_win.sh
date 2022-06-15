615123b2988fc509c3814e82cfdb659857aeb976
Grace Effiong <graceffiong@gmail.com>
2022-06-15 20:02:53 +0000

    task 4

--- 0x18-dynamic_libraries/101-make_me_win.sh
+++ 0x18-dynamic_libraries/101-make_me_win.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-wget -P .. https://raw.githubusercontent.com/davidmusau/alx-low_level_programming/master/0x18-dynamic_libraries/libgiga.so
+wget -P .. https://raw.githubusercontent.com/davidmusau/alx-low_level_programming/master/0x18-dynamic_libraries/libdynamic.so
 export LD_PRELOAD="$PWD/../libgiga.so"
