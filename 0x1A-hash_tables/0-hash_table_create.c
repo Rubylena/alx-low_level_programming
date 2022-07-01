fafc57e157e5b9de3c630f2f1bb88b64dd061c82
Grace Effiong <graceffiong@gmail.com>
2022-07-01 19:33:27 +0000

    hashes

--- 0x1A-hash_tables/0-hash_table_create.c
+++ 0x1A-hash_tables/0-hash_table_create.c
@@ -5,7 +5,7 @@
  * @size: The size of the array.
  *
  * Return: if an error occurs - NULL.
- * 	   otherwise - a pointer to the new hash table.
+ *		otherwise - a pointer to the new hash table.
  */
 
 hash_table_t *hash_table_create(unsigned long int size)
